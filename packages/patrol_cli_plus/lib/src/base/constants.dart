b9d7d6a463f918bc3f121896a7488877db7b6054
ahmednfwela <63286031+ahmednfwela@users.noreply.github.com>
2026-09-05 17:02:30 +0000

    [create-pull-request] automated change

--- packages/patrol_cli_plus/lib/src/base/constants.dart
+++ packages/patrol_cli_plus/lib/src/base/constants.dart
@@ -1,4 +1,4 @@
 // Modified by Bdaya-Dev from the original LeanCode Patrol source (Apache-2.0). See NOTICE.md.
 /// Version of Patrol CLI. Must be kept in sync with pubspec.yaml.
 /// If you update this, make sure that compatibility-table.mdx is updated (if needed)
-const version = '5.5.1';
+const version = '5.5.2';
